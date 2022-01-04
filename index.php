8ba4340522b54d335a9588712eb0f6999dbe6bf8
Alesson Marques <alessonmarques@live.com>
2022-01-04 10:47:42 -0300

    Setting the project base to work with the CMC Api

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+ini_set('error_reporting', E_ALL);
+
+require __DIR__ . '/vendor/autoload.php';
+new \App\Support\Environment();
+
+include_once("cmcapi.php");
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '614e587f6693465b312d90f20edd259a26174cd3',
+    'name' => 'aless/cmcapi',
+  ),
+  'versions' => 
+  array (
+    'aless/cmcapi' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '614e587f6693465b312d90f20edd259a26174cd3',
+    ),
+  ),
+);
